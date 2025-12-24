2e60f143c4f3a062770ae8338c0e0972605dc384
Dwi Laras Setyadita <dwilaras.2022@student.uny.ac.id>
2025-12-24 20:30:52 +0700

    install react and tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 function App() {
   return (
-    <div className="bg-green-600 text-white p-10 text-4xl">
-      Hello World
+    <div className="min-h-screen bg-secondary flex items-center justify-center">
+      <div className="bg-primary p-12 rounded-2xl">
+        <h1 className="text-accent text-4xl font-biryani">
+          SEMANTIC COLOR JALAN 🎨
+        </h1>
+        <p className="text-accent2 font-openSans mt-4">
+          Tailwind v4 CSS-first theme
+        </p>
+      </div>
     </div>
   )
 }
