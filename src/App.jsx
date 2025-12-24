8c75ab4cf21bf2da68d9c25ed82bb94c32e5fe33
Dwi Laras Setyadita <dwilaras.2022@student.uny.ac.id>
2025-12-24 19:09:11 +0700

    install react and tailwind

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-green-600 text-white p-10 text-4xl">
+      Hello World
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./public/**/*.{html,js}"],
+    theme: {
+      extend: {
+        colors: {
+          primary: "#D9D9D9",
+          secondary: "#263652",
+          accent: "#B77E23",
+          lightaccent: "#C2AA71",
+          accent2: "#6F2F06",
+        },
+        fontFamily: {
+          biryani: ["'Biryani'", "sans-serif"],
+          openSans: ['"Open Sans"', 'sans-serif']
+        }
+      },
+    },
+    plugins: [],
+  }
+  
+  
